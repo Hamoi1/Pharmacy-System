c24d82b7cd4097d99074960c3d50e7e82de4e670
Muhammad ismail <ihama9728@gmail.com>
2022-10-16 12:24:28 +0300

    remove Auto complete

--- resources/views/pos/barcode.blade.php
+++ resources/views/pos/barcode.blade.php
@@ -3,6 +3,6 @@
         {{ __('header.barcode') }}
         <i class="fas fa-barcode"></i>
     </label>
-    <input type="text" id="barcode" wire:model.debounce.100ms="barcode" class="form-control" placeholder="{{ __('header.barcode') }}">
+    <input type="text" id="barcode" wire:model.debounce.10ms="barcode" class="form-control" placeholder="{{ __('header.barcode') }}" autocomplete="off">
     @error('barcode') <span class="text-danger"> {{ $message }}</span> @enderror
 </div>
