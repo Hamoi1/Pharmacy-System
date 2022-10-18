9a22de77512aa150cb5874b80140de83159922cb
Hamoi1 <ihama9728@gmail.com>
2022-10-18 20:47:29 +0300

    Update

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,13 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (Auth::user()->role == 1) {
+                    return redirect(RouteServiceProvider::HOME);
+                } elseif (Auth::user()->role == 2) {
+                    return redirect(RouteServiceProvider::Cashire);
+                } else {
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const Cashire = '/pont-of-sale';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
