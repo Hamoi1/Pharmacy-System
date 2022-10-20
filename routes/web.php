e5f94e2753a7a459b5197ce92b880844ae75e8cf
Hamoi1 <ihama9728@gmail.com>
2022-10-20 15:29:48 +0300

    Optimize Routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,13 @@ Route::group([
     'where' => ['lang' => 'en|ckb'],
 ], function () {
     Route::middleware(['auth'])->group(function () {
+        Route::get('/', function () {
+            if (Auth::user()->role === 1) {
+                return redirect()->route('dashboard', app()->getLocale());
+            } else {
+                return redirect()->route('sales', app()->getLocale());
+            }
+        });
         Route::get('/dashboard', App\Http\Controllers\Dashboard::class)->name('dashboard');
         Route::get('/users', App\Http\Controllers\User\Index::class)->name('users');
         Route::get('/products', App\Http\Controllers\Products\Index::class)->name('products');
@@ -18,7 +25,6 @@ Route::group([
         Route::get('/stock-out-procuts', App\Http\Controllers\StockOutProducts::class)->name('StockOutProcuts');
         Route::get('/categorys', App\Http\Controllers\Categorys\Index::class)->name('categorys');
         Route::get('/suppliers', App\Http\Controllers\Supplier\Index::class)->name('suppliers');
-        Route::get('/raport', App\Http\Controllers\Raports\Index::class)->name('raports');
         Route::get('/pont-of-sale', App\Http\Controllers\Pos\Index::class)->name('sales');
         Route::get('/sales', App\Http\Controllers\Sales\Index::class)->name('sales.index');
         Route::get('/sale/view/{id}/{invoice}', App\Http\Controllers\Sales\ViewSale::class)->name('sales.view');
