216b29ed7fc36a0b34d84b948d0d05a3b7a4101e
Hamoi1 <ihama9728@gmail.com>
2022-10-21 17:06:44 +0300

    Fixed

--- resources/views/dashboard/expiry-product.blade.php
+++ resources/views/dashboard/expiry-product.blade.php
@@ -1,28 +1,26 @@
 <div class="col-sm-6 col-lg-3">
     <div class="card">
         <div class="card-body">
-<<<<<<< HEAD
             <span class="fw-bolder fs-3">
                 {{ __('header.ExpiryProducts') }}
             </span>
             <div class="row align-items-center  mt-4">
-=======
-            <div class="d-flex flex-column">
-                <span class="fw-bolder fs-3">
-                    {{ __('header.ExpiryProducts') }}
-                </span>
-              </div>
-            <div class="row align-items-center  mt-3">
->>>>>>> 1089be4fbb3d1853c5012eaf4b5d335e34a88060
-                <div class="col-auto">
-                    <span class="bg-cyan avatar">
-                        <i class="fa-solid fa-box fs-2"></i>
+                <div class="d-flex flex-column">
+                    <span class="fw-bolder fs-3">
+                        {{ __('header.ExpiryProducts') }}
                     </span>
                 </div>
-                <div class="col">
-                    <span class="fs-1 fw-bolder">
-                        {{ number_format($ExpiryCount , 0) }}
-                    </span>
+                <div class="row align-items-center  mt-3">
+                    <div class="col-auto">
+                        <span class="bg-cyan avatar">
+                            <i class="fa-solid fa-box fs-2"></i>
+                        </span>
+                    </div>
+                    <div class="col">
+                        <span class="fs-1 fw-bolder">
+                            {{ number_format($ExpiryCount , 0) }}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
