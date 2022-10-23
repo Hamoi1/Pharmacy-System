df2bf94a1c2b48467f13365ff3bfb31d9a57d710
Muhammad ismail <ihama9728@gmail.com>
2022-10-23 16:32:19 +0300

    Change Variables

--- app/Http/Controllers/Generate/UserReport.php
+++ app/Http/Controllers/Generate/UserReport.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers\Generate;
 
+use App\Models\Products;
+use App\Models\Sales;
 use App\Models\User;
 use Livewire\Component;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Validation\Rules\RequiredIf;
 
 class UserReport extends Component
 {
     protected $listeners = ['generateReport' => 'generateReport'];
-    public $type, $UserId;
+    public $type, $UserId, $User_Name;
     public function reports()
     {
         return [
             __('header.report.Sales'),
             __('header.report.Add Product'),
-            __('header.report.Edit Product'),
         ];
     }
     public function render()
@@ -26,28 +28,46 @@ class UserReport extends Component
     public function generateReport($id)
     {
         $this->UserId = $id;
+        $this->User_Name = User::FindOrFail($id)->name;
     }
     public function done()
     {
+        $this->reset(['type', 'UserId']);
         $this->dispatchBrowserEvent('closeModal');
     }
     public function generate()
     {
         $this->validate(
-            ['type' => 'required'],
+            [
+                'type' => 'required',
+            ],
             [
                 'type.required' => __('validation.required', ['attribute' => __('header.report.index')]),
             ]
         );
         if ($this->type == 0) {
-            $user = User::with('sales')->FindOrFail($this->UserId);
-            $pdf = Pdf::loadView('pdf.sales', ['user' => $user]);
+            $user = User::findOrFail($this->UserId);
+            $sales = Sales::where('user_id', $this->UserId)->latest()->get();
+            if ($sales->count() == 0) {
+                return $this->addError('GlobalError', __('header.report.noData'));
+            }
+            $pdf = Pdf::loadView('pdf.sales', ['user' => $user, 'sales' => $sales]);
+            $this->done();
             return response()->streamDownload(function () use ($pdf) {
                 echo $pdf->output();
-            }, 'sales.pdf');
+            }, $this->User_Name . ' - sales' . '.pdf');
+        } elseif ($this->type == 1) {
+            $user = User::FindOrFail($this->UserId);
+            $products = Products::where('user_id', $this->UserId)->latest()->get();
+            if ($products->count() == 0) {
+                return $this->addError('GlobalError', __('header.report.noData'));
+            }
+            $pdf = Pdf::loadView('pdf.add-products', ['user' => $user, 'products' => $products]);
+            return response()->streamDownload(function () use ($pdf) {
+                echo $pdf->output();
+            }, $this->User_Name . ' - addProduct' . '.pdf');
         } else {
             $this->addError('type', __('validation.required', ['attribute' => __('header.report.index')]));
         }
-        $this->done();
     }
 }
