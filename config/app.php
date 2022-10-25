dc273cfa287f4e27b2d12bf71e7f8bcc13fd6767
Muhammad ismail <ihama9728@gmail.com>
2022-10-25 17:56:33 +0300

    Configuration Milon Barcode

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Milon\Barcode\BarcodeServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -210,6 +211,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Image' => 'Intervention\Image\Facades\Image',
+        'DNS1D' => 'Milon\Barcode\Facades\DNS1DFacade',
+        'DNS2D' => 'Milon\Barcode\Facades\DNS2DFacade',
     ])->toArray(),
 
 ];
