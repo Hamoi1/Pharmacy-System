42be8be78ce6b365a3d4fc95d73c3d7b58efca6f
Muhammad ismail <ihama9728@gmail.com>
2022-10-30 23:17:47 +0300

    Add Top 10 Proudct Sale

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\sale_details;
 use App\Models\User;
 use Livewire\Component;
 use Illuminate\Support\Facades\Gate;
@@ -14,6 +15,7 @@ class Dashboard extends Component
             redirect()->route('sales', app()->getLocale());
         }
         $users = User::withCount('sales')->orderBy('sales_count', 'desc')->take(10)->get();
-        return view('dashboard', ['users' => $users]);
+        $products = sale_details::with('products')->selectRaw('sum(quantity) as total_quantity, product_id')->groupBy('product_id')->orderBy('total_quantity', 'desc')->take(10)->get();
+        return view('dashboard', ['users' => $users, 'products' => $products]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,12 +17,21 @@
                 <livewire:dashboard.total-sales />
                 <livewire:dashboard.total-product />
                 <livewire:dashboard.total-users />
-                <div class="row mt-4 not-reverse">
-                    <h3 class="">{{ __('Top 10 User Sales') }}</h3>
+                <div class="row mt-4 gy-3 not-reverse">
                     <div class="col-12">
+                        <h3 class="">{{ __('Top 10 User Sales') }}</h3>
                         <div class="card">
                             <div class="card-body">
-                                <div id="chart" class="chart-lg"></div>
+                                <div id="chart" class="chart-lg chart"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="col-12">
+                        <h3 class="">{{ __('Top 10 Product Sales') }}</h3>
+                        <div class="card">
+                            <div class="card-body">
+                                <div id="chart-product" class="chart-lg"></div>
                             </div>
                         </div>
                     </div>
@@ -54,7 +63,12 @@
             },
         },
         dataLabels: {
-            enabled: false
+            enabled: true,
+            offsetX: 0,
+            style: {
+                fontSize: '12px',
+                colors: ['#fff']
+            }
         },
         stroke: {
             show: true,
@@ -83,6 +97,60 @@
         }
     };
     var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+
+
+    var Products = @js($products);
+    var options = {
+        series: [{
+            name: 'Quantity Product Sale',
+            data: Products.map(function(product) {
+                return product.total_quantity
+            })
+        }],
+        chart: {
+            height: 350,
+            type: 'bar',
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+                columnWidth: '50%',
+                endingShape: 'rounded'
+            },
+        },
+        dataLabels: {
+            enabled: true,
+            offsetX: 0,
+            style: {
+                fontSize: '12px',
+                colors: ['#fff']
+            }
+        },
+        stroke: {
+            show: true,
+            width: 4,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: Products.map(function(product) {
+                return product.products.name
+            }),
+        },
+        yaxis: {},
+        fill: {
+            opacity: .7,
+            colors: ['#1C2833']
+        },
+        tooltip: {
+            y: {
+                formatter: function(val) {
+                    return val
+                }
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#chart-product"), options);
     chart.render();
 </script>
 
