827ab38a41c882b1143da6a36e734b4e7ca4a8a1
Hamoi1 <ihama9728@gmao.com>
2023-06-02 16:47:03 +0300

    Update Login

--- app/Http/Controllers/Auth/Login.php
+++ app/Http/Controllers/Auth/Login.php
@@ -14,28 +14,21 @@ class Login extends Component
     {
         $this->validateOnly($propertyName);
     }
-    public function rules()
+
+    public $email, $password;
+    public function login()
     {
-        return [
+        $this->validate([
             'email' => 'required|email|regex:/^[a-zA-Z0-9_.+-]+@[a-zA-Z]+\.[a-zA-Z]+$/u|exists:users,email',
             'password' => 'required|min:8'
-        ];
-    }
-    public function messages()
-    {
-        return [
+        ], [
             'email.required' => __('validation.required', ['attribute' => __('header.email')]),
             'email.email' => __('validation.email', ['attribute' => __('header.email')]),
             'email.regex' => __('validation.regex', ['attribute' => __('header.email')]),
             'email.exists' => __('validation.exists', ['attribute' => __('header.email')]),
             'password.required' => __('validation.required', ['attribute' => __('header.password')]),
             'password.min' => __('validation.min.string', ['attribute' => __('header.password'), 'min' => 8]),
-        ];
-    }
-    public $email, $password;
-    public function login()
-    {
-        $this->validate($this->rules(), $this->messages());
+        ]);
         if (auth()->attempt(['email' => $this->email, 'password' => $this->password])) {
             if (auth()->user()->status === 1) {
                 $data = auth()->user()->name . ' Login to System ';
@@ -45,7 +38,8 @@ class Login extends Component
                 auth()->logout();
                 $this->dispatchBrowserEvent('message', ['type' => 'error', 'message' => __('header.Account_not_active')]);
             }
-        } else {
+        }
+        else {
             $this->addError('email', __('header.failed'));
         }
     }
